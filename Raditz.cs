211122a8c5575c89c7b7e0c95ceee660101a592d
Jongtek-23 <72738269+Jongtek-23@users.noreply.github.com>
2022-04-19 08:59:31 +0200

    Add files via upload

---
+++ Raditz.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Reflection;
+
+namespace Raditz
+{
+    internal class Program
+    {
+        static void Reflect(string FilePath)
+        {
+            Assembly dotnetProgram = Assembly.LoadFrom(FilePath);
+            Object[] parameters = new string[] { null };
+            dotnetProgram.EntryPoint.Invoke(null, parameters);
+        }
+
+        static void Main(string[] args)
+        {
+            Reflect(@"C:\Users\jongtek\Documents\Reflection\HelloReflectionWorld\HelloReflectionWorld\bin\x64\Release\HelloReflectionWorld.exe");
+        }
+    }
+}
